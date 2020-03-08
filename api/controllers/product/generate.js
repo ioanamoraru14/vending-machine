5ffde67d6c65ad6f2b814b203a4d7bf487ee12a8
Ioana Moraru <ioanamoraru14@gmail.com>
2020-03-08 20:13:33 +0200

    change products name

--- api/controllers/product/generate.js
+++ api/controllers/product/generate.js
@@ -5,12 +5,12 @@ module.exports = async function(req, res) {
 	let machine_prop = { sold: 0 }
 	let machine = await Machine.create(machine_prop).fetch()
 
-	await Product.create({ name: 'Avira Prime', price: 75, stock: 3, location: 'A1', machine: machine.id })
-    await Product.create({ name: 'Antivirus PRO', price: 35, stock: 3, location: 'A2', machine: machine.id })
-    await Product.create({ name: 'Phantom VPN', price: 50, stock: 5, location: 'B3', machine: machine.id })
-    await Product.create({ name: 'Password ManagerO', price: 20, stock: 3, location: 'C4', machine: machine.id })
+	await Product.create({ name: 'Prime', price: 75, stock: 3, location: 'A1', machine: machine.id })
+    await Product.create({ name: 'Antivirus', price: 35, stock: 3, location: 'A2', machine: machine.id })
+    await Product.create({ name: 'Phantom', price: 50, stock: 5, location: 'B3', machine: machine.id })
+    await Product.create({ name: 'Password', price: 20, stock: 3, location: 'C4', machine: machine.id })
     await Product.create({ name: 'Optimizer', price: 10, stock: 5, location: 'D2', machine: machine.id })
-    await Product.create({ name: 'System Speedup', price: 25, stock: 4, location: 'D4', machine: machine.id })
+    await Product.create({ name: 'Speedup', price: 25, stock: 4, location: 'D4', machine: machine.id })
 
 	return res.ok()
 }
