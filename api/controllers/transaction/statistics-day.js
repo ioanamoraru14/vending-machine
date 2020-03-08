6f2a5744bfc20a5c7772a373f0860c6156851ccc
Ioana Moraru <ioanamoraru14@gmail.com>
2020-03-09 00:00:55 +0200

    reverse days for stats

--- api/controllers/transaction/statistics-day.js
+++ api/controllers/transaction/statistics-day.js
@@ -6,18 +6,19 @@ module.exports = async function(req, res) {
 	today_format = dd +'/'+ mm +'/'+ yyyy;
 
 	let dict = {}
-	dict[today_format] = 0
 
 	var i;
 	let last = today;
-	for (i = 0; i < req.query.nr_days - 1; i++) { 
-		last = new Date(last - (24 * 60 * 60 * 1000));
+	for (i = req.query.nr_days - 1; i >= 0; i--) { 
+		last = new Date(today - (i * 24 * 60 * 60 * 1000));
 		dd = String(last.getDate()).padStart(2, '0');
 		mm = String(last.getMonth() + 1).padStart(2, '0');
 		yyyy = last.getFullYear();
 		last_format = dd +'/'+ mm +'/'+ yyyy;
 		dict[last_format] = 0
 	}
+
+	dict[today_format] = 0
 
 	start = new Date(today.getTime() - (req.query.nr_days * 24 * 60 * 60 * 1000));
 	let transactions = await Transaction.find().where({ "createdAt" : { ">": start.getTime() } });
